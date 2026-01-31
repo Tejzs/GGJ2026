d6a19fcb106badcda43438dd594028f5f734b7e1
Hazra <rd412466@dal.ca>
2026-01-30 20:58:59 -0400

    added base card info and stats

---
+++ Assets/CardInfo.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class AceSpade : MonoBehaviour
+{
+    public int number;
+    public int Suit;
+    public string id;
+
+// 1. card are numbered from 1 ace to 13 king.
+// 2. suit is numbered spade 1, heart 2, club 3, diamond 4.
+// 3. id is first letter of suit followed by number, eg: ace of spades s1.
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
