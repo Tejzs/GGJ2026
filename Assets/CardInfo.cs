6416c0f55c8ef713bb6d3f963d4cc05c5f7bc45b
Hazra <rd412466@dal.ca>
2026-01-30 21:05:37 -0400

    changed id to numerical vale from 1-52

--- Assets/CardInfo.cs
+++ Assets/CardInfo.cs
@@ -4,11 +4,11 @@ public class AceSpade : MonoBehaviour
 {
     public int number;
     public int Suit;
-    public string id;
+    public int id;
 
 // 1. card are numbered from 1 ace to 13 king.
 // 2. suit is numbered spade 1, heart 2, club 3, diamond 4.
-// 3. id is first letter of suit followed by number, eg: ace of spades s1.
+// 3. id is a numeric value from 1-52.
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
